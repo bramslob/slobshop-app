8fca7bc4005b72b5be2f2c48253d7ca33f5be744
Bram Slob <b.slob@solvari.com>
2017-05-18 15:33:04 +0200

    Scaffolding the app, lists -> overview & list -> detail view

---
+++ src/pages/lists/lists.ts
@@ -0,0 +1,31 @@
+import {Component} from "@angular/core";
+import {IonicPage, NavController, NavParams} from "ionic-angular";
+
+/**
+ * Generated class for the ListsPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+@IonicPage({
+    name: 'lists',
+    segment: 'lists'
+})
+@Component({
+    selector: 'page-lists',
+    templateUrl: 'lists.html',
+})
+export class ListsPage {
+
+    list = [
+        {i: 1, name: 'list 1', checked: false}
+    ];
+
+    constructor(public navCtrl: NavController, public navParams: NavParams) {
+    }
+
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad ListsPage');
+    }
+
+}
